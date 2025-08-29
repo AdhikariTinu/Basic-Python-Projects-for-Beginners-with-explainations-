359de6cc1dcbb77820cccf41ab2cdf768020e577
AdhikariTinu <87791142+AdhikariTinu@users.noreply.github.com>
2025-08-29 15:12:11 +0530

    Add files via upload

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,27 +1,29 @@
-import random
-print(f"\nWelcome to the NUMBER guessing game. Please select your lower and upper bound below.")
+# A number guessing game where the user has to guess a randomly generated number within a specified range and a limited number of attempts.
+import random # importing random module to generate random numbers
+print(f"\nWelcome to the NUMBER guessing game. Please select your lower and upper bound below.") # Welcoming the user
 
-num1 = int(input(f"\n Please select your lower bound: "))
-num2 = int(input(f"\n Please select your upper bound: "))
+num1 = int(input(f"\n Please select your lower bound: ")) # Taking lower bound input from user
+num2 = int(input(f"\n Please select your upper bound: ")) # Taking upper bound input from user
 
-print(f"\n You have 7 guesses to find the number between {num1} and {num2}.Let's begin! \n")
+print(f"\n You have 7 guesses to find the number between {num1} and {num2}.Let's begin! \n") # Informing the user about the number of attempts
 
-r_no = random.randint(num1, num2)
-g = 0
-ch = 7
+r_no = random.randint(num1, num2) # Generating a random number between the specified bounds
+g = 0                             # Initializing guess counter
+ch = 7                            # Setting the maximum number of attempts
 
-while g < ch:
-    g += 1
-    guess = int(input(f"\n Enter your guess no. {g}: "))
+# Loop until the user has exhausted their attempts
+while g < ch: # Looping until the user has exhausted their attempts
+    g += 1    # Incrementing the guess counter
+    guess = int(input(f"\n Enter your guess no. {g}: ")) # Taking the user's guess as input
 
-    if guess == r_no:
-        print(f"\n Hurray!,You guessed the right no. that is {r_no}. And guessed it in {g} attempt(s)")
-        break
-    elif g >= ch and guess != r_no:
-        print(f"\n Sorry, Your number was {r_no}, Better luck next time")
+    if guess == r_no:  # Checking if the guess is correct
+        print(f"\n Hurray!,You guessed the right no. that is {r_no}. And guessed it in {g} attempt(s)") # Congratulating the user for the correct guess
+        break # Exiting the loop if the guess is correct
+    elif g >= ch and guess != r_no: # Checking if the user has exhausted their attempts
+        print(f"\n Sorry, Your number was {r_no}, Better luck next time") # Informing the user that they have exhausted their attempts
 
-    elif guess > r_no:
-        print(f"\n Your number is lower than this!")
+    elif guess > r_no: # Checking if the guess is too high
+        print(f"\n Your number is lower than this!") # Giving a hint that the guess is too high
 
-    elif guess < r_no:
-        print(f"\n Your number is higher than this!")
+    elif guess < r_no: # Checking if the guess is too low
+        print(f"\n Your number is higher than this!") # Giving a hint that the guess is too low
